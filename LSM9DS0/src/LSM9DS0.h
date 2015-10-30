d724c93916e9aea905dfc3f62bc768e017b3aaf8
PSanf2 <PatrickSanford@live.com>
2015-10-30 21:27:58 +0000

    Renamed a directory to indicate which board the program is for. Laid the groundwork for a driver to interact with the LSM9DS0 board.
    Signed-off-by: PSanf2 <PatrickSanford@live.com>

---
+++ LSM9DS0/src/LSM9DS0.h
@@ -0,0 +1,17 @@
+#ifndef __LSM9DS0
+#define __LSM9DS0
+
+#include "I2C_IO.h"
+
+namespace PatricksDrivers {
+
+	class LSM9DS0 : public I2C_IO {
+		public:
+			LSM9DS0(unsigned char bus, unsigned char addr);
+			~LSM9DS0();
+	};
+
+}
+
+#endif
+

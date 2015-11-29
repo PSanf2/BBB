9f8dfbb2bd1831cbb946bc7b46ec0ca6a342c6eb
PSanf2 <PatrickSanford@live.com>
2015-11-29 00:41:13 +0000

    Commented out debugging printf calls.
    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- src/LSM9DS0/LSM9DS0.cpp
+++ src/LSM9DS0/LSM9DS0.cpp
@@ -17,9 +17,9 @@ LSM9DS0::LSM9DS0(
 	lsm9ds0MagGain_t magGain,
 	lsm9ds0GyroScale_t gyroScale)
 {
-	printf("\n&_accel_mg_lsb = \t%X", &_accel_mg_lsb);
-	printf("\n&_mag_mgauss_lsb = \t%X", &_mag_mgauss_lsb);
-	printf("\n&_gyro_dps_digit = \t%X", &_gyro_dps_digit);
+	//printf("\n&_accel_mg_lsb = \t%X", &_accel_mg_lsb);
+	//printf("\n&_mag_mgauss_lsb = \t%X", &_mag_mgauss_lsb);
+	//printf("\n&_gyro_dps_digit = \t%X", &_gyro_dps_digit);
 	
 	_bus = bus;
 	_range = accelRange;
@@ -30,14 +30,14 @@ LSM9DS0::LSM9DS0(
 	// I need to perform the setup tasks here.
 	// I'll need to read in a value from select registers, and
 	// populate the _accel_mg_lsb, _mag_mgauss_lsb, and _gyro_dps_digit variables
-	
+	/*
 	printf("\nLSM9DS0 Constructor called.");
 	printf("\n_bus\t= %u", _bus);
 	printf("\n_range\t= %f", _range);
 	printf("\n_gain\t= %f", _gain);
 	printf("\n_scale\t= %f", _scale);
 	printf("\n_err\t= %d", _err);
-	
+	*/
 	// check the board ID for accel/mag
 	I2C_IO* comm;
 	
@@ -48,7 +48,7 @@ LSM9DS0::LSM9DS0(
 	}
 	delete comm;
 	
-	printf("\nLSM9DS0_REGISTER_WHO_AM_I_XM\t== LSM9DS0_XM_ID");
+	//printf("\nLSM9DS0_REGISTER_WHO_AM_I_XM\t== LSM9DS0_XM_ID");
 	
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_GYRO);
 	if (comm->readRegister(LSM9DS0_REGISTER_WHO_AM_I_G) != LSM9DS0_G_ID) {
@@ -57,31 +57,31 @@ LSM9DS0::LSM9DS0(
 	}
 	delete comm;
 	
-	printf("\nLSM9DS0_REGISTER_WHO_AM_I_G\t== LSM9DS0_G_ID");
+	//printf("\nLSM9DS0_REGISTER_WHO_AM_I_G\t== LSM9DS0_G_ID");
 	
 	// start talking w/ the accel/mag device
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_ACCELMAG);
 	// enable accelerometer continous
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG1_XM, 0x67);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG5_XM, 0xF0);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	// enable mag continous
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG7_XM, 0x00);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	// enable temp
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG5_XM, (comm->readRegister(LSM9DS0_REGISTER_CTRL_REG5_XM) | 0x80));
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	// stop talking w/ the device
 	delete comm;
 	
 	// enable gyro
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_GYRO);
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG1_G, 0x0F);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	delete comm;
 	
-	printf("\nAll devices enabled.");
+	//printf("\nAll devices enabled.");
 	
 	// perform the required setup tasks
 	// (why enable before setup?)
@@ -89,14 +89,14 @@ LSM9DS0::LSM9DS0(
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_ACCELMAG);
 	// setup accel
 	tempReg = comm->readRegister(LSM9DS0_REGISTER_CTRL_REG2_XM);
-	printf("\ncomm->get_error() = %d", comm->get_error());
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ntempReg\t= %u", tempReg);
 	tempReg &= ~(0x38);
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ntempReg\t= %u", tempReg);
 	tempReg |= _range;
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ntempReg\t= %u", tempReg);
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG2_XM, tempReg);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	switch (_range) {
 		case LSM9DS0_ACCELRANGE_2G:
 			_accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_2G;
@@ -114,18 +114,18 @@ LSM9DS0::LSM9DS0(
 			_accel_mg_lsb = LSM9DS0_ACCEL_MG_LSB_16G;
 		break;
 	}
-	printf("\n&_accel_mg_lsb\t\t= %X", &_accel_mg_lsb);
-	printf("\n_accel_mg_lsb\t= %f", _accel_mg_lsb);
+	//printf("\n&_accel_mg_lsb\t\t= %X", &_accel_mg_lsb);
+	//printf("\n_accel_mg_lsb\t= %f", _accel_mg_lsb);
 	// setup mag
 	tempReg = comm->readRegister(LSM9DS0_REGISTER_CTRL_REG6_XM);
-	printf("\ncomm->get_error() = %d", comm->get_error());
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ntempReg\t= %u", tempReg);
 	tempReg &= ~(0x60);
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ntempReg\t= %u", tempReg);
 	tempReg |= _gain;
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ntempReg\t= %u", tempReg);
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG6_XM, tempReg);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	switch(_gain) {
 		case LSM9DS0_MAGGAIN_2GAUSS:
 			_mag_mgauss_lsb = LSM9DS0_MAG_MGAUSS_2GAUSS;
@@ -140,20 +140,20 @@ LSM9DS0::LSM9DS0(
 			_mag_mgauss_lsb = LSM9DS0_MAG_MGAUSS_12GAUSS;
 		break;
 	}
-	printf("\n&_mag_mgauss_lsb\t= %X", &_mag_mgauss_lsb);
-	printf("\n_mag_mgauss_lsb\t= %f", _mag_mgauss_lsb);
+	//printf("\n&_mag_mgauss_lsb\t= %X", &_mag_mgauss_lsb);
+	//printf("\n_mag_mgauss_lsb\t= %f", _mag_mgauss_lsb);
 	delete comm;
 	// setup gyro
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_GYRO);
 	tempReg = comm->readRegister(LSM9DS0_REGISTER_CTRL_REG4_G);
-	printf("\ncomm->get_error() = %d", comm->get_error());
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ntempReg\t= %u", tempReg);
 	tempReg &= ~(0x30);
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ntempReg\t= %u", tempReg);
 	tempReg |= _scale;
-	printf("\ntempReg\t= %u", tempReg);
+	//printf("\ntempReg\t= %u", tempReg);
 	comm->writeRegister(LSM9DS0_REGISTER_CTRL_REG4_G, tempReg);
-	printf("\ncomm->get_error() = %d", comm->get_error());
+	//printf("\ncomm->get_error() = %d", comm->get_error());
 	switch(_scale) {
 		case LSM9DS0_GYROSCALE_245DPS:
 			_gyro_dps_digit = LSM9DS0_GYRO_DPS_DIGIT_245DPS;
@@ -165,8 +165,8 @@ LSM9DS0::LSM9DS0(
 			_gyro_dps_digit = LSM9DS0_GYRO_DPS_DIGIT_2000DPS;
 		break;
 	}
-	printf("\n&_gyro_dps_digit\t= %X", &_gyro_dps_digit);
-	printf("\n_gyro_dps_digit\t= %f", _gyro_dps_digit);
+	//printf("\n&_gyro_dps_digit\t= %X", &_gyro_dps_digit);
+	//printf("\n_gyro_dps_digit\t= %f", _gyro_dps_digit);
 	delete comm;
 }
 
@@ -175,8 +175,8 @@ LSM9DS0::~LSM9DS0() {
 }
 
 void LSM9DS0::readAccel() {
-	printf("\n_accel_mg_lsb =\t%f", _accel_mg_lsb);
-	printf("\n&_accel_mg_lsb = \t%X", &_accel_mg_lsb);
+	//printf("\n_accel_mg_lsb =\t%f", _accel_mg_lsb);
+	//printf("\n&_accel_mg_lsb = \t%X", &_accel_mg_lsb);
 	
 	I2C_IO* comm;
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_ACCELMAG);
@@ -202,9 +202,9 @@ void LSM9DS0::readAccel() {
 	accelData.y = yhi;
 	accelData.z = zhi;
 	
-	printf("\naccelData.x =\t%f", accelData.x);
-	printf("\naccelData.y =\t%f", accelData.y);
-	printf("\naccelData.z =\t%f", accelData.z);
+	//printf("\naccelData.x =\t%f", accelData.x);
+	//printf("\naccelData.y =\t%f", accelData.y);
+	//printf("\naccelData.z =\t%f", accelData.z);
 	
 	acceleration.x = accelData.x * _accel_mg_lsb;
 	acceleration.x /= 1000;
@@ -227,8 +227,8 @@ void LSM9DS0::readAccel() {
 }
 
 void LSM9DS0::readMag() {
-	printf("\n_mag_mgauss_lsb =\t%f", _mag_mgauss_lsb);
-	printf("\n&_mag_mgauss_lsb = \t%X", &_mag_mgauss_lsb);
+	//printf("\n_mag_mgauss_lsb =\t%f", _mag_mgauss_lsb);
+	//printf("\n&_mag_mgauss_lsb = \t%X", &_mag_mgauss_lsb);
 	
 	I2C_IO* comm;
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_ACCELMAG);
@@ -254,9 +254,9 @@ void LSM9DS0::readMag() {
 	magData.y = yhi;
 	magData.z = zhi;
 	
-	printf("\nmagData.x =\t%f", magData.x);
-	printf("\nmagData.y =\t%f", magData.y);
-	printf("\nmagData.z =\t%f", magData.z);
+	//printf("\nmagData.x =\t%f", magData.x);
+	//printf("\nmagData.y =\t%f", magData.y);
+	//printf("\nmagData.z =\t%f", magData.z);
 	
 	magnetic.x = magData.x * _mag_mgauss_lsb;
 	magnetic.x /= 1000;
@@ -276,8 +276,8 @@ void LSM9DS0::readMag() {
 }
 
 void LSM9DS0::readGyro() {
-	printf("\n_gyro_dps_digit =\t%f", _gyro_dps_digit);
-	printf("\n&_gyro_dps_digit = \t%X", &_gyro_dps_digit);
+	//printf("\n_gyro_dps_digit =\t%f", _gyro_dps_digit);
+	//printf("\n&_gyro_dps_digit = \t%X", &_gyro_dps_digit);
 	
 	I2C_IO* comm;
 	comm = new I2C_IO(_bus, LSM9DS0_ADDRESS_GYRO);
@@ -303,9 +303,9 @@ void LSM9DS0::readGyro() {
 	gyroData.y = yhi;
 	gyroData.z = zhi;
 	
-	printf("\ngyroData.x =\t%f", gyroData.x);
-	printf("\ngyroData.y =\t%f", gyroData.y);
-	printf("\ngyroData.z =\t%f", gyroData.z);
+	//printf("\ngyroData.x =\t%f", gyroData.x);
+	//printf("\ngyroData.y =\t%f", gyroData.y);
+	//printf("\ngyroData.z =\t%f", gyroData.z);
 	
 	gyroscopic.x = gyroData.x * _gyro_dps_digit;
 	gyroscopic.y = gyroData.y * _gyro_dps_digit;
@@ -334,7 +334,7 @@ void LSM9DS0::readTemp() {
 	
 	tempData = xhi;
 	
-	printf("\ntempData = %i", tempData);
+	//printf("\ntempData = %i", tempData);
 	
 	temperature = 21.0 + (float) tempData / 8;
 	

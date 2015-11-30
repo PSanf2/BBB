4baa54b59950c1a730df46f6c94d238d81bf7390
PSanf2 <PatrickSanford@live.com>
2015-11-30 00:31:59 +0000

    Renamed a directory.
    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- bash/link_Singleton_main.sh
+++ bash/link_Singleton_main.sh
@@ -2,7 +2,7 @@
 
 echo "Building Singleton"
 g++ -o obj/I2C_IO_Singleton.o -c src/lib/I2C_IO_Singleton.cpp
-g++ -o obj/LSM9DS0_Singleton.o -c src/LSM9DS0/LSM9DS0_Singleton.cpp
-g++ -o obj/Singleton_CLI_UI_main.o -c src/LSM9DS0/LSM9DS0_Singleton_CLI_UI_main.cpp
+g++ -o obj/LSM9DS0_Singleton.o -c src/LSM9DS0_Singleton/LSM9DS0_Singleton.cpp
+g++ -o obj/Singleton_CLI_UI_main.o -c src/LSM9DS0_Singleton/LSM9DS0_Singleton_CLI_UI_main.cpp
 g++ -o bin/Singleton_LSM9DS0_CLI obj/*
 echo "Done"

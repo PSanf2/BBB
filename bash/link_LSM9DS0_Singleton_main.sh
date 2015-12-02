271ab5f8b14e6de6f9b983c8c69f6df47846df04
PSanf2 <PatrickSanford@live.com>
2015-12-02 22:26:59 +0000

    Got the executable files to place nice with each other. I can now have multiple boards connected to the same bus, and control them independently.
    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- bash/link_LSM9DS0_Singleton_main.sh
+++ bash/link_LSM9DS0_Singleton_main.sh
@@ -3,6 +3,6 @@
 echo "Building Singleton"
 g++ -o obj/I2C_IO_Singleton.o -c src/lib/I2C_IO_Singleton.cpp
 g++ -o obj/LSM9DS0_Singleton.o -c src/LSM9DS0_Singleton/LSM9DS0_Singleton.cpp
-g++ -o obj/Singleton_CLI_UI_main.o -c src/LSM9DS0_Singleton/LSM9DS0_Singleton_CLI_UI_main.cpp
-g++ -o bin/Singleton_LSM9DS0_CLI obj/*
+g++ -o obj/LSM9DS0_Singleton_CLI_UI_main.o -c src/LSM9DS0_Singleton/LSM9DS0_Singleton_CLI_UI_main.cpp
+g++ -o bin/LSM9DS0_Singleton obj/I2C_IO_Singleton.o obj/LSM9DS0*
 echo "Done"

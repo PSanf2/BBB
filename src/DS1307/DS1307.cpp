e9317fc39aa799dff3ba7c6f270f744bff394da1
PSanf2 <PatrickSanford@live.com>
2015-12-25 18:20:17 -0500

    Got all functions declared in the .h file. Have empty functions for all declarations in the .cpp file waiting to be filled in.
    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- src/DS1307/DS1307.cpp
+++ src/DS1307/DS1307.cpp
@@ -12,8 +12,86 @@ namespace PatricksDrivers {
 		_bus = bus;
 	}
 	
-	DS1307::~DS1307() {
+	struct tm* DS1307::read_time() {
+		struct tm* result;
+		return result;
+	}
+	
+	void DS1307::write_time (struct tm* newDateTime) {
 		
+	}
+	
+	void DS1307::toggle_CH() {
+		
+	}
+	
+	bool DS1307::CH_enabled() {
+		return false;
+	}
+	
+	void DS1307::toggle_mode() {
+		
+	}
+	
+	hourMode_t DS1307::read_mode() {
+		hourMode_t result;
+		return result;
+	}
+	
+	void DS1307::toggle_out() {
+		
+	}
+	
+	out_t DS1307::read_out() {
+		out_t result;
+		return result;
+	}
+	
+	void DS1307::set_rate(rateSelect_t freq) {
+		
+	}
+	
+	rateSelect_t DS1307::read_rate() {
+		rateSelect_t result;
+		return result;
+	}
+	
+	void DS1307::toggle_SQWE() {
+		
+	}
+	
+	bool DS1307::SQWE_enabled() {
+		return false;
+	}
+	
+	/* REQUIRED FUNCTIONS
+	 * Funtions to read/write from the 56 bytes of battery backed RAM on the chip
+	 * 		The RAM is available in addresses 0x08 - 0x3F.
+	 * 		I'll want to be able to write any C++ standard data type to the RAM.
+	 * 		I want to accept a register address, and a variable. Evaluate the variable
+	 * 		to determine it's type, and figure out how many bytes to write. Make sure my
+	 * 		first byte is a valid register to write to, make sure I won't write to an address
+	 * 		beyond 0x3F, and write the bytes to the registers.
+	 * 		I'll need to see if there's some function parameter type witchery that will
+	 * 		allow a function to accept an argument of an arbitrary type. I'll want to use
+	 * 		function templates for this. I may end up using a sizeof function on the
+	 * 		parameter to determine how many bytes to write. This would allow for a function
+	 * 		that doesn't need to use a case statement, and I'd be able to write non-basic
+	 * 		variable types.
+	 * 		For reading functions I'll want to know what type of a variable I'll be
+	 * 		getting from the RAM so I can cast the result before returning it. I'll
+	 * 		accept an address, a type, make sure I don't try to read past 0x3F, cast
+	 * 		the result to the proper type, and retun it.
+	 * 		I'm not going to worry about memory management other than making sure I'm
+	 * 		reading/writing from valid register addresses.
+	 */
+	
+	template <class T*> void DS1307::write_RAM(unsigned char start_addr, T* val) {
+		
+	}
+	
+	template <class T*> T* DS1307::read_RAM(unsigned char start_addr, T* result) {
+		return result;
 	}
 	
 } // namespace
--- src/DS1307/DS1307.h
+++ src/DS1307/DS1307.h
@@ -40,10 +40,8 @@ namespace PatricksDrivers {
 			unsigned char _bus;
 		public:
 			DS1307(unsigned char bus);
-			~DS1307();
-			
-			struct tm read_time();
-			void set_time (struct tm newDateTime);
+			struct tm* read_time();
+			void write_time (struct tm* newDateTime);
 			void toggle_CH();
 			bool CH_enabled();
 			void toggle_mode();
@@ -54,25 +52,8 @@ namespace PatricksDrivers {
 			rateSelect_t read_rate();
 			void toggle_SQWE();
 			bool SQWE_enabled();
-			
-			/* REQUIRED FUNCTIONS
-			 * Funtions to read/write from the 56 bytes of battery backed RAM on the chip
-			 * 		The RAM is available in addresses 0x08 - 0x3F.
-			 * 		I'll want to be able to write any C++ standard data type to the RAM.
-			 * 		I want to accept a register address, and a variable. Evaluate the variable
-			 * 		to determine it's type, and figure out how many bytes to write. Make sure my
-			 * 		first byte is a valid register to write to, make sure I won't write to an address
-			 * 		beyond 0x3F, and write the bytes to the registers.
-			 * 		I'll need to see if there's some function parameter type witchery that will
-			 * 		allow a function to accept an argument of an arbitrary type. I'll want to use
-			 * 		function templates for this.
-			 * 		For reading functions I'll want to know what type of a variable I'll be
-			 * 		getting from the RAM so I can cast the result before returning it. I'll
-			 * 		accept an address, a type, make sure I don't try to read past 0x3F, cast
-			 * 		the result to the proper type, and retun it.
-			 * 		I'm not going to worry about memory management other than making sure I'm
-			 * 		reading/writing from valid register addresses.
-			 */
+			template <class T*> void write_RAM(unsigned char start_addr, T* val);
+			template <class T*> T* read_RAM(unsigned char start_addr, T* result);
 			 
 	}; // class
 	

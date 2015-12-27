3ca85e4572c9df13a5b9274a4314765604471154
PSanf2 <PatrickSanford@live.com>
2015-12-26 19:21:00 -0500

    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- src/DS1307/DS1307_CLI_UI_main.cpp
+++ src/DS1307/DS1307_CLI_UI_main.cpp
@@ -9,6 +9,16 @@ using namespace std;
 // Functions to put things on the screen.
 void printMenu() {
 	printf("\n\t\t-----MAIN MENU-----");
+	printf("\n\t 1) Print Status");
+	printf("\n\t 2) Read Time");
+	printf("\n\t 3) Write Time");
+	printf("\n\t 4) Toggle Clock Halt");
+	printf("\n\t 5) Toggle 12/24 Hour Mode");
+	printf("\n\t 6) Toggle Output Control");
+	printf("\n\t 7) Toggle Square-wave Enable");
+	printf("\n\t 8) Set Square Wave Frequency");
+	printf("\n\t 9) Memory Read");
+	printf("\n\t10) Memory Write");
 	printf("\n\t 0) Quit");
 	printf("\nInput selection ");
 }
@@ -60,6 +70,81 @@ int main(int argc, char* argv[]) {
 		system("clear");
 		
 		switch (menu_choice) {
+			
+			case 1:
+				printf("\n-----STATUS-----");
+				
+				if (RTClock.CH_enabled())
+					printf("\n\tClock Halt: ENABLED");
+				else
+					printf("\n\tClock Halt: DISABLED");
+				
+				if (RTClock.read_mode() == PatricksDrivers::TWENTYFOURHOURMODE)
+					printf("\n\t24 Hour Mode");
+				else
+					printf("\n\t12 Hour Mode");
+				
+				if (RTClock.read_out() == PatricksDrivers::LOW)
+					printf("\n\tOutput Contorl: LOW");
+				else
+					printf("\n\tOutput Contorl: HIGH");
+				
+				if (RTClock.SQWE_enabled())
+					printf("\n\tSquare Wave: ENABLED");
+				else
+					printf("\n\tSquare Wave: DISABLED");
+				
+				switch (RTClock.read_rate()) {
+					case PatricksDrivers::ONE_HZ:
+						printf("\n\tFrequency: 1Hz");
+					break;
+					case PatricksDrivers::FOUR_KHZ:
+						printf("\n\tFrequency: 4.096kHz");
+					break;
+					case PatricksDrivers::EIGHT_KHZ:
+						printf("\n\tFrequency: 8.192kHz");
+					break;
+					case PatricksDrivers::THIRTYTWO_KHZ:
+						printf("\n\tFrequency: 32.768kHz");
+					break;
+				} // switch (RTClock.read_rate())
+			break; // case 1
+			
+			case 2:
+				
+			break;
+			
+			case 3:
+				
+			break;
+			
+			case 4:
+				
+			break;
+			
+			case 5:
+				
+			break;
+			
+			case 6:
+				
+			break;
+			
+			case 7:
+				
+			break;
+			
+			case 8:
+				
+			break;
+			
+			case 9:
+				
+			break;
+			
+			case 10:
+				
+			break;
 			
 			case 0:
 				; // do nothing

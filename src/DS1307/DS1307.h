1a2c50b7672f11896fc2f7acaaa382e47bf225c6
PSanf2 <PatrickSanford@live.com>
2016-01-03 19:06:36 -0500

    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- src/DS1307/DS1307.h
+++ src/DS1307/DS1307.h
@@ -59,32 +59,6 @@ namespace PatricksDrivers {
 			 
 	}; // class
 	
-	/* REQUIRED FUNCTION TEMPLATES
-	 * 
-	 * Function templates needs to be defined in the header file.
-	 * Trying to define them in the cpp file won't compile.
-	 * 
-	 * Funtions to read/write from the 56 bytes of battery backed RAM on the chip
-	 * 		The RAM is available in addresses 0x08 - 0x3F.
-	 * 		I'll want to be able to write any C++ standard data type to the RAM.
-	 * 		I want to accept a register address, and a variable. Evaluate the variable
-	 * 		to determine it's type, and figure out how many bytes to write. Make sure my
-	 * 		first byte is a valid register to write to, make sure I won't write to an address
-	 * 		beyond 0x3F, and write the bytes to the registers.
-	 * 		I'll need to see if there's some function parameter type witchery that will
-	 * 		allow a function to accept an argument of an arbitrary type. I'll want to use
-	 * 		function templates for this. I may end up using a sizeof function on the
-	 * 		parameter to determine how many bytes to write. This would allow for a function
-	 * 		that doesn't need to use a case statement, and I'd be able to write non-basic
-	 * 		variable types.
-	 * 		For reading functions I'll want to know what type of a variable I'll be
-	 * 		getting from the RAM so I can cast the result before returning it. I'll
-	 * 		accept an address, a type, make sure I don't try to read past 0x3F, cast
-	 * 		the result to the proper type, and retun it.
-	 * 		I'm not going to worry about memory management other than making sure I'm
-	 * 		reading/writing from valid register addresses.
-	 */
-	
 	template <class T> void DS1307::write_RAM(unsigned char start_addr, T val) {
 		// make sure my starting address is valid
 		if (start_addr < 0x08)
@@ -124,13 +98,13 @@ namespace PatricksDrivers {
 		// clear the current value in *result
 		*result = 0;
 		
-		// create an array to hold the results off the chip
+		// create an array to hold the bytes off the chip
 		unsigned char* vals = new unsigned char[sizeof(*result)];
 		
 		// read the proper number of bytes off the chip
 		Device->read(_bus, DS1307_DEV_ADDR, start_addr, sizeof(*result), vals);
 		
-		// or shift each byte of the read values into the result variable
+		// shift and OR each byte into the result
 		for (int i = 0; i < sizeof(*result); i++) {
 			*result |= (vals[i] << (8 * i));
 		}

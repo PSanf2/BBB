9a0ca797e84619f9e9c6fcfa0fe7d1e491a0a103
PSanf2 <PatrickSanford@live.com>
2016-01-03 21:11:38 -0500

    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- bash/link_ADS1X15_Singleton_main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building ADS1X15_Singleton main"
-g++ -o obj/I2C_IO_Singleton.o -c src/lib/I2C_IO_Singleton.cpp
-g++ -o obj/ADS1X15_Singleton.o -c src/ADS1X15_Singleton/ADS1X15_Singleton.cpp
-g++ -o obj/ADS1X15_Singleton_CLI_UI_main.o -c src/ADS1X15_Singleton/ADS1X15_Singleton_CLI_UI_main.cpp
-g++ -o bin/ADS1X15_Singleton obj/I2C_IO_Singleton.o obj/ADS1X15*
-echo "Done"

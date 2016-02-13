4af905d7b9bf050845393df3e2fed9aee68074db
PSanf2 <PatrickSanford@live.com>
2016-02-13 07:10:20 -0500

    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- bash/link_PCA9685_main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building PCA9685 main"
-g++ -o obj/I2C_IO_Singleton.o -c src/lib/I2C_IO_Singleton.cpp
-g++ -o obj/PCA9685.o -c src/PCA9685/PCA9685.cpp
-g++ -o obj/PCA9685_CLI_UI_main.o -c src/PCA9685/PCA9685_CLI_UI_main.cpp
-g++ -o bin/PCA9685 obj/I2C_IO_Singleton.o obj/PCA9685*
-echo "Done"

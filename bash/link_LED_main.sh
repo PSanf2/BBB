61202276e8ee291d7ff61c867192196b515f4233
PSanf2 <PatrickSanford@live.com>
2016-02-21 13:10:59 -0500

    Signed-off-by: PSanf2 <PatrickSanford@live.com>

--- bash/link_LED_main.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Building LED main..."
-./bash/compile_BBIO_Lite.sh
-g++ -o obj/LED.o -c src/LED/LED.cpp
-g++ -o obj/LED_CLI_UI_main.o -c src/LED/LED_CLI_UI_main.cpp
-g++ -o bin/LED obj/BBIO_Lite* obj/LED*
-echo "Done Building LED main"
